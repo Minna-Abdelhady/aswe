97a60ce19629de645bc0f5ce04189f00ec7856e8
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2024-02-26 21:16:06 +0200

    Update UserController.java

--- project/src/main/java/com/example/aswe/project/controllers/UserController.java
+++ project/src/main/java/com/example/aswe/project/controllers/UserController.java
@@ -1,5 +1,16 @@
 package com.example.aswe.project.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.aswe.project.repositories.UserRepository;
+
+@RestController
+@RequestMapping("/User")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
     
 }
