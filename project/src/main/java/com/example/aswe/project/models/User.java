5f5a4549ba26ccf25cc20e870c0ad0c493d3a509
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2024-02-28 11:49:08 +0200

    User class

--- project/src/main/java/com/example/aswe/project/models/User.java
+++ project/src/main/java/com/example/aswe/project/models/User.java
@@ -1,5 +1,59 @@
 package com.example.aswe.project.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String FName;
+    private String LName;
+    private String Email;
+    private String Password;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFName() {
+        return this.FName;
+    }
+
+    public void setFName(String FName) {
+        this.FName = FName;
+    }
+
+    public String getLName() {
+        return this.LName;
+    }
+
+    public void setLName(String LName) {
+        this.LName = LName;
+    }
+
+    public String getEmail() {
+        return this.Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPassword() {
+        return this.Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+
 }
