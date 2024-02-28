9fa5286b966f5d44d032c0243e0dcc4a2b2704a4
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2024-02-28 12:11:22 +0200

    Profile controller with no HTMLs

--- project/src/main/java/com/example/aswe/project/controllers/UserController.java
+++ project/src/main/java/com/example/aswe/project/controllers/UserController.java
@@ -1,9 +1,15 @@
 package com.example.aswe.project.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.example.aswe.project.models.User;
 import com.example.aswe.project.repositories.UserRepository;
 
 @RestController
@@ -11,6 +17,20 @@ import com.example.aswe.project.repositories.UserRepository;
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping("profile/{userId}")
+    public ModelAndView get1User(@PathVariable("userId") Integer userId){
+        ModelAndView mav = new ModelAndView("profile.html");
+        List<User> users = this.userRepository.findAll();
+        for (User user : users) {
+            if(user.getId() == userId){
+                mav.addObject("users", users);
+                return mav;
+            }
+        }
+        mav.addObject("error", "User not found");
+        return mav;
+    }
 
     
 }
