47f2b1043e947f6335875a917abce3251e99f2cc
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2024-03-05 15:59:10 +0200

    Commenting all cart code to run

--- project/src/main/java/com/example/aswe/project/models/CartItem.java
+++ project/src/main/java/com/example/aswe/project/models/CartItem.java
@@ -1,33 +1,33 @@
-package com.example.aswe.project.models;
+// package com.example.aswe.project.models;
 
-import org.springframework.data.annotation.Id;
+// import org.springframework.data.annotation.Id;
 
-import jakarta.annotation.sql.DataSourceDefinition;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+// import jakarta.annotation.sql.DataSourceDefinition;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.FetchType;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.ManyToOne;
+// import jakarta.persistence.OneToOne;
+// import jakarta.persistence.Table;
 
 
-@Entity
-@Table(name="cart_item")
+// @Entity
+// @Table(name="cart_item")
 
-public class CartItem {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="order_detailed_id")
-    private long id;
-    private int quantity;
-    private double totalPrice;
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name="shopping_cart_id",referencedColumnName = "shopping_cart_id")
-    private shoppingcart ShoppingCart;
-    @OneToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name="product_id",referencedColumnName = "product_id")
-    private product Product;
-} 
+// public class CartItem {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     @Column(name="order_detailed_id")
+//     private long id;
+//     private int quantity;
+//     private double totalPrice;
+//     @ManyToOne(fetch = FetchType.EAGER)
+//     @JoinColumn(name="shopping_cart_id",referencedColumnName = "shopping_cart_id")
+//     private shoppingcart ShoppingCart;
+//     @OneToOne(fetch = FetchType.EAGER)
+//     @JoinColumn(name="product_id",referencedColumnName = "product_id")
+//     private product Product;
+// } 
--- project/src/main/java/com/example/aswe/project/models/shoppingcart.java
+++ project/src/main/java/com/example/aswe/project/models/shoppingcart.java
@@ -1,60 +1,60 @@
-package com.example.aswe.project.models;
+// package com.example.aswe.project.models;
 
-import java.util.Set;
+// import java.util.Set;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
+// import jakarta.persistence.CascadeType;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.FetchType;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.OneToMany;
+// import jakarta.persistence.OneToOne;
 
-@Entity
-public class shoppingcart {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long userID;
-    private int productID;
-    private int quantity;
-    private double subtotal;
-    @OneToOne(fetch=FetchType.EAGER)
-    @JoinColumn(name="User_id",referencedColumnName = "User_id")
-     private User user;
-     @OneToMany(cascade = CascadeType.ALL,mappedBy = "cart")
-     private Set <CartItem> cartItem;
-    public long getUserID() {
-        return this.userID;
-    }
+// @Entity
+// public class shoppingcart {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private long userID;
+//     private int productID;
+//     private int quantity;
+//     private double subtotal;
+//     @OneToOne(fetch=FetchType.EAGER)
+//     @JoinColumn(name="User_id",referencedColumnName = "User_id")
+//      private User user;
+//      @OneToMany(cascade = CascadeType.ALL,mappedBy = "cart")
+//      private Set <CartItem> cartItem;
+//     public long getUserID() {
+//         return this.userID;
+//     }
 
-    public void setUserID(long userID) {
-        this.userID = userID;
-    }
+//     public void setUserID(long userID) {
+//         this.userID = userID;
+//     }
 
-    public long getProductID() {
-        return this.productID;
-    }
+//     public long getProductID() {
+//         return this.productID;
+//     }
 
-    public void setProductID(int productID) {
-        this.productID = productID;
-    }
+//     public void setProductID(int productID) {
+//         this.productID = productID;
+//     }
 
-    public int getQuantity() {
-        return this.quantity;
-    }
+//     public int getQuantity() {
+//         return this.quantity;
+//     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
+//     public void setQuantity(int quantity) {
+//         this.quantity = quantity;
+//     }
 
-    public double getSubtotal() {
-        return this.subtotal;
-    }
+//     public double getSubtotal() {
+//         return this.subtotal;
+//     }
 
-    public void setSubtotal(double subtotal) {
-        this.subtotal = subtotal;
-    }
+//     public void setSubtotal(double subtotal) {
+//         this.subtotal = subtotal;
+//     }
 
-}
+// }
