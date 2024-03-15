a5f1743e92ef3d7f153fd067ca2dfb0314726548
mohamed2106505 <140180824+mohamed2106505@users.noreply.github.com>
2024-03-15 22:00:27 +0200

    Update ProductRepository.java

--- project/src/main/java/com/example/aswe/project/repositories/ProductRepository.java
+++ project/src/main/java/com/example/aswe/project/repositories/ProductRepository.java
@@ -1,23 +1,9 @@
 package com.example.aswe.project.repositories;
 
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.stereotype.Repository;
-
-import com.example.aswe.project.models.User;
 import com.example.aswe.project.models.product;
 
-@Repository
-public class ProductRepository extends JpaRepository<product, Long>  {
-
-    public void save(product newProduct) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
-    }
-
-    public List<product> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
-    }
+public interface productRepository extends JpaRepository<product,Long> {
     
 }
