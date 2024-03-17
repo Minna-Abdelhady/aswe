8d7c547f33a14bc823f023e7eecfaedc626aae3a
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2024-03-17 19:52:10 +0200

    TRYYYYYY

--- project/src/main/java/com/example/aswe/project/repositories/UserRepository.java
+++ project/src/main/java/com/example/aswe/project/repositories/UserRepository.java
@@ -1,7 +1,7 @@
 package com.example.aswe.project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.aswe.project.models.User;
