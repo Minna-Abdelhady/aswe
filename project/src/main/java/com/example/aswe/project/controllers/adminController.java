b260b60253a6580c4c84060799cc290c66ecf6fd
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2024-03-17 22:51:27 +0200

    Edit users (ADMIN)

--- project/src/main/java/com/example/aswe/project/controllers/adminController.java
+++ project/src/main/java/com/example/aswe/project/controllers/adminController.java
@@ -2,10 +2,12 @@ package com.example.aswe.project.controllers;
 
 import java.util.List;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.aswe.project.models.Admin;
 import com.example.aswe.project.models.User;
@@ -13,6 +15,9 @@ import com.example.aswe.project.repositories.AdminRepository;
 import com.example.aswe.project.repositories.UserRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
@@ -39,6 +44,39 @@ public class AdminController {
         List<User> users = this.userRepository.findAll();
         mav.addObject("users", users);
         return mav;
+    }
+
+    @GetMapping("edit-user/{userId}")
+    public ModelAndView editProfile(@PathVariable("userId") Integer userId) {
+        ModelAndView mav = new ModelAndView("/html/admin/edit-user.html");
+        User newUser = this.userRepository.findByid(userId);
+        if (newUser != null) {
+            mav.addObject("user", newUser);
+            return mav;
+        }
+        ModelAndView errorMav = new ModelAndView("error.html");
+        errorMav.addObject("errorMessage", "User not found");
+        return errorMav;
+    }
+
+    @PostMapping("edit-user/{userId}")
+    public RedirectView saveProfile(@PathVariable("userId") int userId, @ModelAttribute User updatedUser) {
+        User user = this.userRepository.findByid(userId);
+        if (!user.getFName().equals(updatedUser.getFName())) {
+            user.setFName(updatedUser.getFName());
+        }
+        if (!user.getLName().equals(updatedUser.getLName())) {
+            user.setLName(updatedUser.getLName());
+        }
+        if (!user.getEmail().equals(updatedUser.getEmail())) {
+            user.setEmail(updatedUser.getEmail());
+        }
+        if (!user.getPassword().equals(updatedUser.getPassword())) {
+            String encoddedPassword = BCrypt.hashpw(updatedUser.getPassword(), BCrypt.gensalt(12));
+            user.setPassword(encoddedPassword);
+        }
+        this.userRepository.save(user);
+        return new RedirectView("/Admin/List-Users");
     }
 
     // @GetMapping("Add-User")
