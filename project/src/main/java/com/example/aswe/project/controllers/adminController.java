cffaa888bd0840a0d3d18b4a30abb7bae4f78b48
Toka2104550 <125702984+Toka2104550@users.noreply.github.com>
2024-03-18 11:35:26 +0200

    ..

--- project/src/main/java/com/example/aswe/project/controllers/adminController.java
+++ project/src/main/java/com/example/aswe/project/controllers/adminController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
