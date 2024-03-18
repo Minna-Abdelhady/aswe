c6108034d40cf6d67da980d4b501cb24d64a0530
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2024-03-18 10:25:51 +0200

    User types

--- project/src/main/java/com/example/aswe/project/controllers/UserController.java
+++ project/src/main/java/com/example/aswe/project/controllers/UserController.java
@@ -7,7 +7,6 @@ import javax.validation.constraints.NotBlank;
 
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
