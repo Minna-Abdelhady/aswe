40f2901ab8fe832435f6972d01f02163b53f1016
Minna-Abdelhady <125457724+Minna-Abdelhady@users.noreply.github.com>
2024-05-10 11:09:59 +0300

    CSS not good

--- project/src/main/resources/static/js/AdminNavBar.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("AdminNavBar.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("AdminNavBar").innerHTML = data;
-    });
-});
