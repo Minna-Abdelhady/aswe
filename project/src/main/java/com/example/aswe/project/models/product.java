1c3075227fcd9088a0052702416b6ae72076e222
mohamed2106505 <140180824+mohamed2106505@users.noreply.github.com>
2024-05-13 23:33:24 +0300

    product crud

--- project/src/main/java/com/example/aswe/project/models/product.java
+++ project/src/main/java/com/example/aswe/project/models/product.java
@@ -1,104 +1,115 @@
 package com.example.aswe.project.models;
+import jakarta.persistence.*;
+import java.util.Date;
 
-import java.util.List;
 
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
+
 
 @Entity
 @Table(name="product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int productId;
-    private String productName;
-    private String productDescription;
-    private Double productPrice;
-    private String status;
+    private int id;
+    private String name;
+    private String brand;
+    private String category;
+    private double price;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private Date createdAt;
+    private String ImgFileName;
 
-    // private byte[] productImg;
-    // private String productImgUrl;
+        public Product() {}
 
-    // @OneToMany
-    // private CartItemsRepository cartItemsRepository;
 
-    public Product() {
 
+    public Product(int id, String name, String brand, String category, double price, String description, Date createdAt, String ImgFileName) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.category = category;
+        this.price = price;
+        this.description = description;
+        this.ImgFileName = ImgFileName;
     }
+
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public String getImgFileName() {
+        return this.ImgFileName;
+    }
+
+    public void setImgFileName(String ImgFileName) {
+        this.ImgFileName = ImgFileName;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            ", brand='" + getBrand() + "'" +
+            ", category='" + getCategory() + "'" +
+            ", price='" + getPrice() + "'" +
+            ", description='" + getDescription() + "'" +
+            ", ImgFileName='" + getImgFileName() + "'" +
+            "}";
+    }
+
+
     
-    public Product(String productName, Double productPrice) {
-        this.productName = productName;
-        this.productPrice = productPrice;
-    }
 
-    public int getProductId() {
-        return productId;
-    }
 
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
 
-    public String getProductName() {
-        return productName;
-    }
 
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public String getProductDescription() {
-        return productDescription;
-    }
-
-    public void setProductDescription(String productDescription) {
-        this.productDescription = productDescription;
-    }
-
-    public Double getProductPrice() {
-        return productPrice;
-    }
-
-    public void setProductPrice(Double productPrice) {
-        this.productPrice = productPrice;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-    // public CartItemsRepository getcartItemsRepository() {
-    //     return cartItemsRepository;
-    // }
-
-    // public void setCartItemsRepository(CartItemsRepository cartItemsRepository) {
-    //     this.cartItemsRepository = cartItemsRepository;
-    // }
-    // public byte[] getProductImg() {
-    //     return productImg;
-    // }
-
-    
-    // public void setProductImg(byte[] productImg) {
-    //     this.productImg = productImg;
-    // }
-
-    // public String getProductImgUrl() {
-    //     return productImgUrl;
-    // }
-
-    // public void setProductImgUrl(String productImgUrl) {
-    //     this.productImgUrl = productImgUrl;
-    // }
-
-    public static List<Product> findAll() {
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
-    }
 
 }
