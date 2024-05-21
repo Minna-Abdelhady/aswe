0116963769559efcb6e8b20f10ad53d92420c645
aya2372002 <125696175+aya2372002@users.noreply.github.com>
2024-05-21 23:29:13 +0300

    carttest

--- project/src/test/java/com/example/aswe/project/CartTest.java
+++ project/src/test/java/com/example/aswe/project/CartTest.java
@@ -1,8 +1,22 @@
 package com.example.aswe.project;
 
+import static org.mockito.Mockito.mock;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.aswe.project.controllers.CartController;
+import com.example.aswe.project.models.CartProducts;
 import com.example.aswe.project.models.ShoppingCart;
+import com.example.aswe.project.models.User;
+import com.example.aswe.project.repositories.UserRepository;
 
 public class CartTest {
     private ShoppingCart shoppingcart=new ShoppingCart();
-    
+    private CartProducts cartproducts= new CartProducts();
+    private User user;
+    private UserRepository userRepository=mock(UserRepository.class);
+     
+    @Autowired
+    private CartController cartcontroller;
+
 }
