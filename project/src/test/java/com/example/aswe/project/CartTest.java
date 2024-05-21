00f6ec1ae1335336541ace33511e47e542601672
aya2372002 <125696175+aya2372002@users.noreply.github.com>
2024-05-21 23:36:20 +0300

    testaddtocart

--- project/src/test/java/com/example/aswe/project/CartTest.java
+++ project/src/test/java/com/example/aswe/project/CartTest.java
@@ -1,7 +1,10 @@
 package com.example.aswe.project;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.aswe.project.controllers.CartController;
@@ -18,5 +21,20 @@ public class CartTest {
      
     @Autowired
     private CartController cartcontroller;
+
+    //set up preconditions before each test method
+    @BeforeEach
+    public void TestaddToCart(){
+         shoppingcart= new ShoppingCart();
+         cartproducts=new CartProducts();
+         cartproducts.setId(1);
+         cartproducts.setName("t11");
+         cartproducts.setPrice(1100);
+         cartproducts.setQuantity(5);
+         shoppingcart.addProductToCart(cartproducts);
+
+         //verify that the product was added successfully
+         assertEquals(1,shoppingcart.getCartProductsList().size());
+    }
 
 }
